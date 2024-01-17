724a2a7113d98db70574dc5d09bbea31f152427d
banelengwane <banelengwane@gmail.com>
2024-01-17 16:10:47 +0200

    creating rating stars

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <StartRating maxRating={5} />
-    <StartRating maxRating={10} />
-    <StartRating />
   </React.StrictMode>
 );
 
