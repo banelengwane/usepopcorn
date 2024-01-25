a15d22ab3b4a267b3222e6697c5221a68368ae9c
banelengwane <banelengwane@gmail.com>
2024-01-25 12:07:03 +0200

    Configuring Enter key to focus on the search bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import './index.css'
 import StarRating from './StarRating'
 
@@ -152,7 +152,24 @@ function Logo(){
 }
 
 function Search ({query, setQuery}) {
+  const inputEl = useRef(null)
 
+  useEffect(() => {
+
+    const callback = (e) => {
+
+      if(document.activeElement === inputEl.current) return;
+
+      if(e.code === 'Enter'){
+        inputEl.current.focus();
+        setQuery('')
+      }
+    }
+
+    document.addEventListener('keydown', callback)
+    
+    return () => document.addEventListener('keydown', callback)
+  }, [])
   return (
     <input
       className="search"
@@ -160,6 +177,7 @@ function Search ({query, setQuery}) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   )
 }
